c81825a84964006f7548797feea9beb47f24dfe5
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-02-06 11:21:01 -0800

    add random and trending options

--- src/index.js
+++ src/index.js
@@ -10,12 +10,47 @@ function getGiphy(search) {
 
   request.addEventListener("loadend", function() {
     const response = JSON.parse(this.responseText);
-    console.log(response);
     if (this.status === 200) {
       printElements(response, search);
     } else {
       // there's a new argument
       printError(this, response, search);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+function getGiphy1() {
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=25&offset=0&rating=g&lang=en`;
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements1(response);
+    } else {
+      // there's a new argument
+      printError1(this, response);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+function getGiphy2() {
+  let request = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&limit=25&offset=0&rating=g&lang=en`;
+
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      printElements2(response);
+    } else {
+      // there's a new argument
+      printError2(this, response);
     }
   });
 
@@ -29,8 +64,15 @@ function printError(request, apiResponse, search) {
   document.querySelector('#showResponse').innerText = `There was an error accessing GIPHY ${search}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
 }
 
+function printError1(request, apiResponse, search) {
+  document.querySelector('#showTranding').innerText = `There was an error accessing GIPHY ${search}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+
+function printError2(request, apiResponse, search) {
+  document.querySelector('#showRandom').innerText = `There was an error accessing GIPHY ${search}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+
 function printElements(response) {
-  console.log(response.data[0].images.downsized.url);
   document.querySelector('#showResponse').src = response.data[0].images.downsized.url;
 }
 
@@ -41,7 +83,29 @@ function handleFormSubmission(event) {
   getGiphy(search);
 }
 
+function printElements1(response) {
+  document.querySelector('#showTranding').src = response.data[0].images.downsized.url;
+}
+
+function handleFormSubmission1(event) {
+  event.preventDefault();
+  getGiphy1();
+}
+
+function printElements2(response) {
+  console.log(response.data.images.downsized.url);
+  document.querySelector('#showRandom').src = response.data.images.downsized.url;
+}
+
+function handleFormSubmission2(event) {
+  event.preventDefault();
+  getGiphy2();
+}
+
+
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
+  document.getElementById('tranding').addEventListener("click", handleFormSubmission1);
+  document.getElementById('random').addEventListener("click", handleFormSubmission2);
 });
 
